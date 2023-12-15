4e2d55c559a30028669583bd13110fb462fe8d34
Abhi <abhipotfode05@gmail.com>
2023-12-15 11:48:21 +0530

    Leetcode 15 dec added

---
+++ Leetcode 15 december/binaryTreePruning.java
@@ -0,0 +1,17 @@
+package com.java.dsa;
+
+import javax.swing.tree.TreeNode;
+
+public class binaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null) {
+            return null;
+        }
+        return root;
+    }
+}
---
+++ Leetcode 15 december/stringFromBinaryTree.java
@@ -0,0 +1,18 @@
+package com.java.dsa;
+import java.util.*;
+
+public class stringFromBinaryTree {
+    public String tree2str(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        if (root.left == null && root.right == null) {
+            return root.val + "";
+        }
+        if (root.right == null) {
+            return root.val + "(" + tree2str(root.left) + ")";
+        }
+        return root.val + "(" + tree2str(root.left) + ")(" + tree2str(root.right) + ")";
+
+    }
+}
